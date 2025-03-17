d83712c2685d6058288434e4a59a4c13705e28d8
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-03-17 21:19:31 +0200

    Update install_odoo_ubuntu.sh

--- install_odoo_ubuntu.sh
+++ install_odoo_ubuntu.sh
@@ -180,7 +180,7 @@ echo "Creating custom addons directory..."
 sudo mkdir $OE_HOME/custom
 sudo mkdir $OE_HOME/custom/addons
 cd /usr/src/
-git clone https://github.com/hrmuwanika/odoo_apps.git
+git https://github.com/hrmuwanika/CybroAddons.git
 cp -rf odoo_apps/* $OE_HOME/custom/addons
 
 echo "Creating enterprise addons directory..."
